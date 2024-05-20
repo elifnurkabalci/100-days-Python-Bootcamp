fa3cb3efe7012e9897414e004cb7f1509f4643ec
Elifnur KABALCI <79353848+elifnurkabalci@users.noreply.github.com>
2024-05-20 23:10:02 +0300

    Add files via upload

---
+++ caesar-cipher-3-start/main.py
@@ -0,0 +1,27 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+def caesar(text, shift, direction):
+  if direction == "encode":
+    new_text = ""
+    for letter in text:
+      position = alphabet.index(letter)
+      new_position = position + shift
+      new_text += alphabet[new_position]
+    print(f"The encoded text is {new_text}")
+    
+  elif direction == "decode":
+    new_text = ""
+    for letter in text:
+      position = alphabet.index(letter)
+      new_position = position - shift
+      new_text += alphabet[new_position]
+    print(f"The decoded text is {new_text}")
+  
+
+caesar(text,shift,direction)
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
---
+++ caesar-cipher-4-start/main.py
@@ -0,0 +1,33 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    position = alphabet.index(char)
+    if position + shift > len(alphabet):
+      position = (position + shift) % len(alphabet)
+    if not char.isalpha():
+      end_text += char
+    else:
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
+option = "yes"
+while option == "yes":
+  print(logo)
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  option = input("If you want to restarting type 'yes' otherwise 'no' ").lower()
+  
